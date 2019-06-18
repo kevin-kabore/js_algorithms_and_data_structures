30be63c2006d4f52c00a2124e68f92af7bb0bbe3
Kevin Kabore <kevin.s.kabore@gmail.com>
2019-06-18 00:29:28 -0700

    Adds inefficient priority queue: replace later

--- dijkstra/index.js
+++ dijkstra/index.js
@@ -17,3 +17,23 @@ class WeightedGraph {
     });
   }
 }
+
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+  enqueue(val, priority) {
+    this.values.push({
+      val,
+      priority
+    });
+    this.sort();
+  }
+
+  dequeue(val) {
+    return this.values.shift(); // remove from beginning
+  }
+  sort() {
+    this.values.sort((a, b) => a.priority - b.priority);
+  }
+}
